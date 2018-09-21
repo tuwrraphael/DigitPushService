8b98a75a53545f07290815e24371f512c94cf640
Raphael <raphael.hauk@racing.tuwien.ac.at>
2018-09-21 15:38:43 +0200

    move project from https://github.com/tuwrraphael/PushServer

--- DigitPushService/Startup.cs
+++ DigitPushService/Startup.cs
@@ -1,31 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using PushServer.Configuration;
+using PushServer.PushConfiguration.EntityFramework.Extensions;
+using PushServer.AzureNotificationHub;
+using PushServer.WebPush;
+using PushServer.WebPushApiClient;
 
 namespace DigitPushService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
             Configuration = configuration;
+            HostingEnvironment = environment;
+            environment.WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment HostingEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = $"Data Source={HostingEnvironment.WebRootPath}\\App_Data\\pushservice.db";
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddPushServer()
+                .AddConfigurationStore(builder =>
+                            builder.UseSqlite(connectionString,
+                                sql => sql.MigrationsAssembly(migrationsAssembly)))
+                .AddAzureNotificationHub(v => {
+                })
+                .AddWebPush(v =>
+                {
+                    v.Issuer = "DigitPushService";
+                    v.PrivateKey = Configuration["VapidPrivateKey"];
+                    v.PublicKey = Configuration["VapidPublicKey"];
+                    v.Subject = Configuration["WebPushSubject"];
+                });
+            services.AddOptions();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = Configuration["ServiceIdentityUrl"];
+                    options.Audience = "push";
+                    options.RequireHttpsMetadata = false;
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("User", builder =>
+                {
+                    builder.RequireAuthenticatedUser();
+                    builder.RequireClaim("scope", "push.user");
+                });
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Service", builder =>
+                {
+                    builder.RequireClaim("scope", "push.service");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +98,8 @@ namespace DigitPushService
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseCors("CorsPolicy");
             app.UseMvc();
         }
     }
